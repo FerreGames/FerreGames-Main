4aeecc5f08327bf8b4bbb836b4c1e68595403ce9
FerreGames <61000625+FerreGames@users.noreply.github.com>
2021-07-15 12:58:02 +0200

    Update warn.js

--- Commands/warn.js
+++ Commands/warn.js
@@ -2,13 +2,13 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) =>{
 
-    if (!message.member.hasPermission("MANAGE_NICKNAMES")) return message.reply("Do you have the right perms");
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Do you have the right perms");
 
     if (!args[0]) return message.reply("Enter a valid user");
 
     if (!args[1]) return message.reply("Please provide a valid reason");
 
-    if (warnUser.hasPermission("MANAGE_NICKNAMES")) return message.reply("You can't warn a Moderator");
+    if (warnUser.hasPermission("KICK_MEMBERS")) return message.reply("You can't warn a Moderator");
 
     if (!warnUser) return message.reply("No valid user found");
 
