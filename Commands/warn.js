c27a9e6909ec06dbf761291bd0d7a85e22b65f57
FerreGames <61000625+FerreGames@users.noreply.github.com>
2021-07-15 13:36:15 +0200

    Update warn.js

--- Commands/warn.js
+++ Commands/warn.js
@@ -38,9 +38,6 @@ module.exports.run = async (client, message, args) => {
         .setFooter("Ferre Games©", client.user.displayAvatarURL())
         .setTimestamp()
         .setColor("#0000ff")
-        // .setColor("#ff0000")
-        // .setFooter(message.member.displayName, message.author.displayAvatarURL)
-        .setTimestamp()
         .setDescription(`**Gewarnd:** ${warnUser} (${warnUser.id})
         **Warning door:** ${message.author}
         **Redenen: ** ${reason}`)
