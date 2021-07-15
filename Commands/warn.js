238998f61e1b9f5c349d06e6236b0b504f884393
FerreGames <61000625+FerreGames@users.noreply.github.com>
2021-07-15 14:18:38 +0200

    Update warn.js

--- Commands/warn.js
+++ Commands/warn.js
@@ -66,7 +66,7 @@ module.exports.run = async (client, message, args) => {
         .setTimestamp()
         .setColor("#00bcff")
 
-    message.warnUser.send(embedDM);
+    message.warnUser(embedDM);
 
 }
 
