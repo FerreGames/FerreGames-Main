5e1d3a40ba1e9b85bf67ce6c8b0a5e83bdc81c3d
FerreGames <61000625+FerreGames@users.noreply.github.com>
2021-11-28 21:15:53 +0100

    Create moderator.js

---
+++ Commands/moderator.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    var modEmbed = new discord.MessageEmbed()
+
+    .setTitle(`Moderator Application | Fewy`)
+    .setDescription(`Hello, this message explains very briefly what exactly we expect from you as a moderator, if you meet those requirements you can fill out the message format.`)
+    .setAuthor(`${user}`, `${userAvatar}`)
+    .setFooter("Fewy©", client.user.displayAvatarURL())
+    .setTimestamp()
+    .setColor("#0000ff")
+
+    channel.send(modEmbed);
+
+}
+
+module.exports.help= {
+    name: "moderator"
+}
